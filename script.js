4a1c26fd679ed8ad3bace2dad53b2bc71cb294aa
RuxCastillo <rubenldc1412@gmail.com>
2024-06-10 21:51:29 -0600

    probando el dom con el json

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ foto.addEventListener("click", alertFun);
 
 function alertFun() {
     console.log("le diste click en la foto");
-}
+} 
+
+const carta = document.querySelector(".card");
+const work = document.querySelector(".work");
+
+console.log(work);
+
+
+const jsonFilePath = "./Data Base/works-projects.json";
+
+let jsonDataWork = {};
+
+fetch(jsonFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al cargar el archivo JSON");
+        }
+        return response.json();
+    })
+    .then(data => {
+        jsonDataWork = data;
+        console.log(jsonDataWork);
+    })
+    .catch(error => {
+        console.error("Hubo un problema con la oepración de fetch:", error)
+    });
+
+
+
