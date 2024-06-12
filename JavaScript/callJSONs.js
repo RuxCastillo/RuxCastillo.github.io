be13b63940300086691ad6fec8b9c8535852114b
RuxCastillo <rubenldc1412@gmail.com>
2024-06-11 21:29:22 -0600

    probando el tener las cosas en carpetas separadas.

--- JavaScript/callJSONs.js
+++ JavaScript/callJSONs.js
@@ -1,16 +1,3 @@
-const foto = document.querySelector("#perfil-picture");
-
-foto.addEventListener("click", alertFun);
-
-function alertFun() {
-    console.log("le diste click en la foto");
-    board.innerHTML = "";
-} 
-
-const carta = document.querySelector(".card");
-const work = document.querySelector(".work");
-
-console.log(work);
 
 // Aqui va el agregar los json que tengo.
 
@@ -68,40 +55,3 @@ fetch(jsonFilePathPractice)
     });
 
 // Aqui se acaba el traer las tarjetas.
-
-const board = document.querySelector(".board");
-
-const works = document.querySelector(".works-projects");
-
-works.addEventListener(
-    "click", 
-    function () {
-        generarTarjetas(jsonDataWork)
-    }
-    );
-
-function generarTarjetas(arr) {
-    let resultado = "";
-
-    const totalTarjetas = arr.length;
-
-    for (let i = 0; i < arr.length; i++) {
-        let addHTML = (
-            `<div class="card ${i}">
-                <div class="tarjeta ${i}">
-                    <h2>${arr[i].title}</h2>
-                    <img />
-                    <p>${arr[i].palabrasclave}</p>
-                    <button>Ver Codigo</button>
-                    <button>Ver mas...</button>
-                </div>
-            </div>`
-
-        )
-
-        resultado += addHTML;
-    }
-
-    console.log(resultado)
-    board.innerHTML = resultado;
-}
