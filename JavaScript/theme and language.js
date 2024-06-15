b43eb7ff8493eaf4dd72b32e96c8c260cf642846
RuxCastillo <rubenldc1412@gmail.com>
2024-06-14 19:55:21 -0600

    parece funcionar bien ya el cambio de color (faltan detalles)

--- JavaScript/theme and language.js
+++ JavaScript/theme and language.js
@@ -1,19 +1,35 @@
 const theme = document.querySelector("#moon");
 const language = document.querySelector("#language");
 const body = document.querySelector("#body");
+let colorActual = "white";
+
 
 theme.addEventListener("click", cambioTheme);
 language.addEventListener("click", cambioLanguage);
 
 
 function cambioTheme() {
-    console.log("le diste click a cambio theme")
-    theme.setAttribute("src", "./Img/icons/sun.png")
-    body.style.backgroundImage = "url(../Img/darktheme.jpg"
-    body.style.color = "white"
-    console.log(fondo)
+    console.log(colorActual)
+
+    if ( colorActual === "white") {
+        colorActual = "black"    
+        console.log("le diste click a cambio theme")
+        theme.setAttribute("src", "./Img/icons/sun.png")
+        body.style.backgroundImage = "url(../Img/darktheme.jpg"
+        body.style.color = "white"
+       
+    } else {
+        colorActual = "white"
+        theme.setAttribute("src", "./Img/icons/luna.png")
+        body.style.backgroundImage = "url(./Img/mamol%20fondo.jpg"
+        body.style.color = "black"
+    }
 }
 
 function cambioLanguage() {
     console.log("le diste click a cambio language")
+}
+
+function temaActual() {
+    return theme.getAttribute("src");
 }
