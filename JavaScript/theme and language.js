e6fdabb227894abfaf43668745287ec78ebaec24
RuxCastillo <rubenldc1412@gmail.com>
2024-06-14 19:30:38 -0600

    agregando imagenes y empezando con el dark theme

---
+++ JavaScript/theme and language.js
@@ -0,0 +1,18 @@
+const theme = document.querySelector("#moon");
+const language = document.querySelector("#language");
+const body = document.querySelector("#body");
+
+theme.addEventListener("click", cambioTheme);
+language.addEventListener("click", cambioLanguage);
+
+
+function cambioTheme() {
+    console.log("le diste click a cambio theme")
+    theme.setAttribute("src", "./Img/icons/sun.png")
+    body.style.backgroundImage = "url(../Img/black.png"
+    console.log(fondo)
+}
+
+function cambioLanguage() {
+    console.log("le diste click a cambio language")
+}
