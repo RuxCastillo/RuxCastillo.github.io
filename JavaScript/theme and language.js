41435385227b2724bb2295ab22a2786900f2f0f1
RuxCastillo <rubenldc1412@gmail.com>
2024-06-16 02:51:08 -0600

    primera version de cambio del idioma, ya sirve el color y cambio del boton language.

--- JavaScript/theme and language.js
+++ JavaScript/theme and language.js
@@ -3,7 +3,10 @@ const language = document.querySelector("#language");
 const body = document.querySelector("#body");
 const imgProfile = document.querySelector("#profile img")
 const oldReddit = document.querySelector("#reddit")
+const allIcons = document.getElementsByClassName("cambio")
 let colorActual = "white";
+let idiomaActual = "en";
+
 
 
 theme.addEventListener("click", cambioTheme);
@@ -16,29 +19,49 @@ function cambioTheme() {
     if ( colorActual === "white") {
         colorActual = "black"    
         console.log("le diste click a cambio theme")
-        theme.setAttribute("src", "./Img/icons/sun.png")
-        language.setAttribute("src", "./Img/icons/enwhite.png")
-        imgProfile.setAttribute("src", "./Img/Perfilbackgroundtransparent2.png")
-        oldReddit.setAttribute("src", "./Img/icons/gridwhite.png")
-        body.style.backgroundImage = "url(../Img/darktheme.jpg"
+        body.style.backgroundImage = "url(../Img/darktheme.jpg)"
+
+        for (let j = 0; j < allIcons.length; j++) {
+        elcambio = allIcons[j].getAttribute("src").slice(0, -4)
+        elcambio += "white.png"
+        console.log(elcambio)
+        allIcons[j].setAttribute("src", elcambio)
+        }
 
         body.style.color = "white"
        
     } else {
         colorActual = "white"
-        theme.setAttribute("src", "./Img/icons/luna.png")
-        language.setAttribute("src", "./Img/icons/idioma.png")
-        imgProfile.setAttribute("src", "./Img/Perfilbackgroundtransparent3.png")
+
+        for (let j = 0; j < allIcons.length; j++) {
+        elcambio = allIcons[j].getAttribute("src").slice(0, -9)
+        elcambio += ".png"
+        console.log(elcambio)
+        allIcons[j].setAttribute("src", elcambio)
+        }
         body.style.backgroundImage = "url(./Img/mamol%20fondo.jpg"
-        oldReddit.setAttribute("src", "./Img/icons/gridblack.png")
         body.style.color = "black"
     }
 }
 
 function cambioLanguage() {
     console.log("le diste click a cambio language")
+    let arreglandoColor = colorActual;
+
+    let str = (arreglandoColor === "white")?  ".png" : "white.png"
+    console.log(arreglandoColor)
+
+    if (idiomaActual === "en") {
+        idiomaActual = "es"
+        language.setAttribute("src", `./Img/icons/es${str}`)
+    } else {
+        idiomaActual = "en"
+        language.setAttribute("src", `./Img/icons/en${str}`)
+    }
+
 }
 
 function temaActual() {
     return theme.getAttribute("src");
+
 }
