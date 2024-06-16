dea8dc92bf88719cedee0cbde955b97ab09c1661
RuxCastillo <rubenldc1412@gmail.com>
2024-06-16 01:00:43 -0600

    solo el archivo nuevo

---
+++ JavaScript/verMas.js
@@ -0,0 +1,60 @@
+const pagina = document.querySelector("#body");
+const vermas = document.querySelector("")
+let = cualBase
+let = loQueHabiaBoard
+
+function abreMas(lugar, elId) {
+
+    console.log(lugar, elId)
+
+    if (lugar === 'practica') {
+        cualBase = jsonDataPractice[elId]
+    }else if (lugar === 'certificados') {
+        cualBase = jsonDataCertifications[elId]
+    } else {
+        cualBase = jsonDataWork[elId]
+    }
+
+    console.log(cualBase)
+    cambiandoElBoard(cualBase)
+}
+
+function cambiandoElBoard(placeholder) {
+    loQueHabiaBoard = board.innerHTML
+    board.innerHTML = "";
+
+    board.innerHTML = (`
+        <div id="newBoard">
+            <div onClick="board.innerHTML = loQueHabiaBoard">
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    `)
+    
+    dj
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
