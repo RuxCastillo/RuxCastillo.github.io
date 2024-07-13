8cc29774774abd2c811ca486c975b88585ca977b
RuxCastillo <rubenldc1412@gmail.com>
2024-07-12 19:58:48 -0600

    arreglando imagenes y modificando texto de practice

--- JavaScript/objeto.js
+++ JavaScript/objeto.js
@@ -5,9 +5,8 @@ const enEs = [
     "aboutMe": "About Me.",
     "ingenieria": "Engineering Student in Software Development.",
     "abajoIngenieria": "Full-Stack developer with focus in Front-End.",
-    "aboutMeText1": "I really enjoy learning and constant challenges that is why I immersed myself in the world of programming, within this I am focusing on front end development but I am also acquiring knowledge of the backend. HTML, CSS, Flexbox and Grid, JavaScript, React, SQL , PostgreSQL, Git, GitHub, DSA (data structure and algorithms) are my work tools for web development, I love how everything is so exact when we write code. I am currently an engineering student and I also study a lot on my own developing myself in this field to be constantly improving every day.",
-
-    "aboutMeText2": "Furthermore, I like to collaborate with other people and consider that learning from others is one of the best ways in which one can learn new things and also retain information. From colleagues you can learn useful things on a daily basis that are not mentioned in the books. A team can always go further than a single person working alone. My goal now is to work on large projects that solve problems in our society and generate an impact on people's well-being.",
+    "aboutMeText1": "I enjoy learning and constant challenges because of this I chose programming, I am acquiring full-stack knowledge with preference to front end, HTML, CSS, JavaScript, React, SQL, PostgreSQL, GitHub, DSA are my work tools for the development of my practices and projects. I am currently an engineering student and in addition to dedicating time outside of school to self-study.",
+    "aboutMeText2": "I like to collaborate with other people and I consider that learning from others is one of the best ways in which one can learn new things, from colleagues you can learn useful things on a daily basis that are not mentioned in books. A team can always go further than a single person working alone, my goal now is to work on large projects that solve problems, generating an impact on people's well-being.",
     "works-projects": "Projects."
     },
     {
@@ -16,8 +15,8 @@ const enEs = [
     "aboutMe": "Acerca de mí.",
     "ingenieria": "Estudiante de Ingeniería en Desarrollo de Software.",
     "abajoIngenieria": "Desarrollador Full-Stack enfocado en Front-End",
-    "aboutMeText1": "Disfruto mucho de aprender y constantes desafíos por eso me sumergí en el mundo de la programación, dentro de este me estoy enfocando en el desarrollo front end pero también estoy adquiriendo conocimientos del backend.HTML, CSS, Flexbox y Grid, JavaScript, React, SQL, PostgreSQL, Git, GitHub, DSA (estructura de datos y algoritmos) son mis herramientas de trabajo para el desarrollo web, me encanta el cómo todo es tan exacto a la hora que escribimos código. Actualmente estoy estudiante la ingeniería y además estudio mucho por mi cuenta desarrollándome en este campo para estar en constante mejora todos los días." ,
-    "aboutMeText2": "Además, me gusta colaborar con otras personas y considera que aprender de otros es de las mejores formas en que uno puede aprender cosas nuevas y además retener la información, de los compañeros se pueden aprender cosas útiles en el día a día que no se mencionan en los libros. Un equipo de trabajo siempre podrá llegar más lejos que una sola persona trabajando por su cuenta, mi objetivo ahora es trabajar en proyectos grandes que resuelvan problemas de nuestra sociedad y generar impacto en el bienestar de las personas.",
+    "aboutMeText1": "Disfruto de aprender y desafíos constantes debido a esto elegí la programación, estoy adquiriendo conocimientos full-stack con preferencia a front end, HTML, CSS, JavaScript, React, SQL, PostgreSQL, GitHub, DSA han son mis herramientas de trabajo para el desarrollo de mis prácticas y proyectos. Actualmente soy estudiante la ingeniería y además de dedicarle tiempo fuera de la escuela a estudio autodidacta." ,
+    "aboutMeText2": "Me gusta colaborar con otras personas y considero que aprender de otros es de las mejores formas en que uno puede aprender cosas nuevas, de los compañeros se pueden aprender cosas útiles en el día a día que no se mencionan en los libros. Un equipo de trabajo siempre podrá llegar más lejos que una sola persona trabajando por su cuenta, mi objetivo ahora es trabajar en proyectos grandes que resuelvan problemas generando impacto en el bienestar de las personas.",
     "works-projects": "Proyectos."
     }
 ]
@@ -31,7 +30,6 @@ const workProjects = [
         "palabras": "Con mucho cariño y para mejorar mi habilidad lo hice Vanilla, considero hacerlo de la forma difícil me ayudo a aprender mucho y mostrarme mis áreas de oportunidad.",
         "codeButton": "https://github.com/RuxCastillo/RuxCastillo.github.io",
         "pagina": "https://ruxcastillo.github.io/",
-        //"verMas": "https://search.brave.com/search?q=google&source=desktop"
 
     }
 ]
@@ -41,7 +39,7 @@ const certifications = [
         "type": "study",
         "id": "0",
         "title": "UVEG (Universidad en progreso)",
-        "img": "./Img/cards/Certifications/universidad.png",
+        "img": "./Img/cards/Certifications/universidad.jpg",
         "link": "https://uveg.edu.mx/index.php/es/",
         "palabrasClave": "Universidad Virtual del Estado de Guanajuato. Estudiante de Ingeniería en Desarrollo de Software"
     },
@@ -49,7 +47,7 @@ const certifications = [
         "type": "certifications",
         "id": "1",
         "title": "The Complete 2024 Web Development Bootcamp",
-        "img": "./Img/cards/Certifications/2024 bootcamp Dra Angela Yu.png",
+        "img": "./Img/cards/Certifications/2024 bootcamp Dra Angela Yu.jpeg",
         "link": "https://www.udemy.com/certificate/UC-316f00d4-2d2f-419d-b893-3312791d1d84/",
         "palabrasClave": "HTML, CSS, Flexbox, Grid, Js, DOM, jQuery, Node.js, Express, EJS, Git, APIs, PostgreSQL y React"
     },
@@ -76,92 +74,92 @@ const practice = [
     {
         "type": "practice",
         "id": "0",
-        "title": "La bandera de Laos",
+        "title": "CSS",
         "img": "./Img/cards/Practice/1practice.png",
-        "palabras": "Mi primer mini practica de CSS",
+        "palabras": "CSS básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/1%20Basic%20Proyect%20CSS/index.html"
 
     },
     {
         "type": "practice",
         "id": "1",
-        "title": "Practicando float en CSS",
+        "title": "CSS",
         "img": "./Img/cards/Practice/2practice.png",
-        "palabras": "Practicando usando float y clear de CSS",
+        "palabras": "Float",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/2%20Practice%20Float%20CSS/index.html"
 
     },
     {
         "type": "practice",
         "id": "2",
-        "title": "Grid y @media",
+        "title": "CSS",
         "img": "./Img/cards/Practice/3practice.png",
-        "palabras": "Aquí practique grid y @media en un diseño sencillo",
+        "palabras": "Grid y @media",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/3%20Pratice%20Grid%20and%20Media/index.html"
 
     },
     {
         "type": "practice",
         "id": "3",
-        "title": "Haciendo una pintura con Grid",
+        "title": "CSS",
         "img": "./Img/cards/Practice/4practice.png",
-        "palabras": "En esta practica hice una pintura del artista Mondrian, para practicar mi CSS ",
+        "palabras": "Hice una pintura del artista Piet Mondrian",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/4%20Practice%20Grid%20Mondrian%20Painting"
     },
     {
         "type": "practice",
         "id": "4",
-        "title": "Haciendo mi primera pagina con Bootstrap",
+        "title": "CSS",
         "img": "./Img/cards/Practice/5practice.png",
-        "palabras": "Aquí aprendí a usar bootstrap, hice una pagina completa y aprendí a lo crucial que es usar frameworks para desarrollar rápido.",
+        "palabras": "Bootstrap",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/5%20Practice%20Bootstrap"
     },
     {
         "type": "practice",
         "id": "5",
-        "title": "Mi primer encuentro con el DOM",
+        "title": "DOM",
         "img": "./Img/cards/Practice/6practice.png",
-        "palabras": "Aquí comencé a explorar el mundo del DOM agregando funcionalidad a las paginas web",
+        "palabras": "DOM básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/5%20Practice%20Bootstrap"
     },
     {
         "type": "practice",
         "id": "6",
-        "title": "Tocando instrumentos con DOM",
+        "title": "DOM",
         "img": "./Img/cards/Practice/7practice.png",
-        "palabras": "En esta practica fue un refuerzo al usar el DOM de forma similar a la pagina anterior pero mejorado",
+        "palabras": "DOM básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/7%20Practice%20DOM"
     },
     {
         "type": "practice",
         "id": "7",
-        "title": "Entrando al mundo de jQuery",
+        "title": "jQuery",
         "img": "./Img/cards/Practice/8practice.png",
-        "palabras": "En esta practica vi los beneficios de usar jQuery el como es mas rápido usarlo que el DOM vanilla",
+        "palabras": "jQuery básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/8%20Practice%20JQuery"
     },
     {
         "type": "practice",
         "id": "8",
-        "title": "Haciendo un proyecto de QR con Node",
+        "title": "Node",
         "img": "./Img/cards/Practice/9practice.png",
-        "palabras": "Realize un pequeño proyecto para ver el funcionamiento de Node, permitiéndome por primera vez salir del navegador y tener mi primer proyecto con backend",
+        "palabras": "Convertidor de URL a QR",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/1%20Practice%20Node%20with%20a%20QR%20Project"
     },
     {
         "type": "practice",
         "id": "9",
-        "title": "Mi primer acercamiento a usar Express",
+        "title": "Node",
         "img": "./Img/cards/Practice/10practice.png",
-        "palabras": "Aquí me introduje un poco mas a el backend, usando express para este proyecto haciendo una pagina sencilla pero que me permitió mejorar mi conocimiento del backend",
+        "palabras": "Express básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/2%20Practice%20Express%20with%20Secret%20Project"
     },
     {
         "type": "practice",
         "id": "10",
-        "title": "En esta actividad aprendí a como usar EJS",
+        "title": "Node",
         "img": "./Img/cards/Practice/11practice.png",
-        "palabras": "Usando EJS aprendí a como hacer html en donde podemos usar js para cambiarlas antes de renderizar y mandarlas al cliente.",
+        "palabras": "EJS básico",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/3%20Practice%20EJS"
     },
 ]
@@ -172,92 +170,92 @@ const practiceEn = [
     {
         "type": "practice",
         "id": "0",
-        "title": "The flag of Laos",
+        "title": "CSS",
         "img": "./Img/cards/Practice/1practice.png",
-        "palabras": "My first CSS mini practice",
+        "palabras": "Basic CSS",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/1%20Basic%20Proyect%20CSS/index.html"
 
     },
     {
         "type": "practice",
         "id": "1",
-        "title": "Practicing float in CSS",
+        "title": "CSS",
         "img": "./Img/cards/Practice/2practice.png",
-        "palabras": "Practicing using CSS float and clear",
+        "palabras": "Float",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/2%20Practice%20Float%20CSS/index.html"
 
     },
     {
         "type": "practice",
         "id": "2",
-        "title": "Grid y @media",
+        "title": "CSS",
         "img": "./Img/cards/Practice/3practice.png",
-        "palabras": "Here practice grid and @media in a simple design",
+        "palabras": "Grid y @media",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/blob/main/1%20Frontend/3%20Pratice%20Grid%20and%20Media/index.html"
 
     },
     {
         "type": "practice",
         "id": "3",
-        "title": "Making a painting with Grid",
+        "title": "CSS",
         "img": "./Img/cards/Practice/4practice.png",
-        "palabras": "In this practice I made a painting by the artist Mondrian, to practice my CSS",
+        "palabras": "I made a painting by the artist Piet Mondrian",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/4%20Practice%20Grid%20Mondrian%20Painting"
     },
     {
         "type": "practice",
         "id": "4",
-        "title": "Making my first page with Bootstrap",
+        "title": "CSS",
         "img": "./Img/cards/Practice/5practice.png",
-        "palabras": "Here I learned how to use bootstrap, I made a complete page and I learned how crucial it is to use frameworks to develop fast",
+        "palabras": "Bootstrap",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/5%20Practice%20Bootstrap"
     },
     {
         "type": "practice",
         "id": "5",
-        "title": "My first encounter with the DOM",
+        "title": "DOM",
         "img": "./Img/cards/Practice/6practice.png",
-        "palabras": "Here I started exploring the world of the DOM by adding functionality to web pages",
+        "palabras": "Basic Dom",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/5%20Practice%20Bootstrap"
     },
     {
         "type": "practice",
         "id": "6",
-        "title": "Playing instruments with DOM",
+        "title": "DOM",
         "img": "./Img/cards/Practice/7practice.png",
-        "palabras": "In this practice it was a reinforcement by using the DOM in a similar way to the previous page but improved",
+        "palabras": "Basic DOM",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/7%20Practice%20DOM"
     },
     {
         "type": "practice",
         "id": "7",
-        "title": "Entering the world of jQuery",
+        "title": "jQuery",
         "img": "./Img/cards/Practice/8practice.png",
-        "palabras": "In this practice I saw the benefits of using jQuery, how it is faster to use than the vanilla DOM",
+        "palabras": "Basic jQuery",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/1%20Frontend/8%20Practice%20JQuery"
     },
     {
         "type": "practice",
         "id": "8",
-        "title": "Making a QR project with Node",
+        "title": "Node",
         "img": "./Img/cards/Practice/9practice.png",
-        "palabras": "I made a small project to see how Node works, allowing me to leave the browser for the first time and have my first project with a backend",
+        "palabras": "URL to QR converter",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/1%20Practice%20Node%20with%20a%20QR%20Project"
     },
     {
         "type": "practice",
         "id": "9",
-        "title": "My first approach to using Express",
+        "title": "Node",
         "img": "./Img/cards/Practice/10practice.png",
-        "palabras": "Here I introduced myself a little more to the backend, using express for this project making a simple page but which allowed me to improve my knowledge of the backend",
+        "palabras": "Basic Express",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/2%20Practice%20Express%20with%20Secret%20Project"
     },
     {
         "type": "practice",
         "id": "10",
-        "title": "In this activity I learned how to use EJS",
+        "title": "Node",
         "img": "./Img/cards/Practice/11practice.png",
-        "palabras": "Using EJS I learned how to make html where we can use js to change them before rendering them and sending them to the client",
+        "palabras": "Basic EJS",
         "codeButton": "https://github.com/RuxCastillo/Practice-Courses/tree/main/2%20Backend/3%20Practice%20EJS"
     },
 ]
@@ -267,7 +265,7 @@ const certificationsEn = [
         "type": "study",
         "id": "0",
         "title": "UVEG (University in progress)",
-        "img": "./Img/cards/Certifications/universidad.png",
+        "img": "./Img/cards/Certifications/universidad.jpg",
         "link": "https://uveg.edu.mx/index.php/es/",
         "palabrasClave": "Universidad Virtual del Estado de Guanajuato. Engineering student in software development"
     },
@@ -275,7 +273,7 @@ const certificationsEn = [
         "type": "certifications",
         "id": "1",
         "title": "The Complete 2024 Web Development Bootcamp",
-        "img": "./Img/cards/Certifications/2024 bootcamp Dra Angela Yu.png",
+        "img": "./Img/cards/Certifications/2024 bootcamp Dra Angela Yu.jpeg",
         "link": "https://www.udemy.com/certificate/UC-316f00d4-2d2f-419d-b893-3312791d1d84/",
         "palabrasClave": "Dr. Angela Yu. HTML, CSS, Flexbox, Grid, Js, DOM, jQuery, Node.js, Express, EJS, Git, APIs, PostgreSQL and React"
     },
