e491bb3eb3b8c949c335c621ecf51e872e8fc2af
RuxCastillo <rubenldc1412@gmail.com>
2024-07-25 02:22:12 -0600

    clases definidas

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,45 +1,43 @@
 class Observer {
-    constructor(lang, theme, device, callbacks){
+    constructor(lang, theme, device, article, callbacks){
         this.lang = lang;
         this.theme = theme;
         this.device = device;
+        this.article = article;
 
         if(callbacks) {
-            this.callbacks.isDark = isDark;
-            this.callbacks.isEng = isEng;
-            this.callbacks.isMobile = isMobile;
+            this.isDark = callbacks.isDark;
+            this.isEng = callbacks.isEng;
+            this.isMobile = callbacks.isMobile;
+            this.changeNotify = callbacks.changeNotify;
         }
+
+        const lis = document.querySelectorAll("header li");
+        
+        for(let li of lis) {
+        li.addEventListener("click", function() {pagina.selectedSection(li.innerText)});
+}
     }
     changeLang() {
-        lang = !this.lang;
-        this.isDark(lang);
+        this.lang = !this.lang;
+        this.isEng(this.lang);
     }
     changeTheme() {
-        theme = !this.theme;
-        this.isDark(theme);
+        this.theme = !this.theme;
+        this.isDark(this.theme);
     }
     changeDevice() {
-        device = !this.device;
-        this.isMobile(device);
+        console.log(this.device)
+        this.device = !this.device;
+        this.isMobile(this.device);
+    }
+    selectedSection(str) {
+        this.article = str;
+        this.changeNotify(this.article)
     }
 }
 
-class Articles {
-    constructor(section, callback) {
-        this.section = section
-
-        if(callback) {
-            this.callbacks.now = now;
-        }
-    }
-    selectedSection(li) {
-        section = li;
-        this.changeNotify(section);
-    }
-
-}
-
-const pagina = new Observer(true, false, false, {
+const pagina = new Observer(true, false, false, "About Me", {
     isDark(bool) {
         console.log("cambio a " + bool + " el theme")
     },
@@ -48,11 +46,38 @@ const pagina = new Observer(true, false, false, {
     },
     isMobile(bool) {
         console.log("cambio a " + bool + " el dispositivo")
+    },
+    changeNotify(str) {
+        console.log("cambio la seccion a " + str)
     }
 })
 
-const sections = new Articles("aboutMe", {
-    changeNotify(li) {
-        console.log("cambio la seccion a " + li)
+class Controladores {
+    constructor(memory, newRequest, callbacks) {
+        this.memory = memory;
+        this.newRequest = newRequest;
+        
+        if(callbacks) {
+            this.erase = callbacks.erase;
+            this.action = callbacks.action;
+            this.compare = callbacks.compare;
+            this.save = callbacks.save;
+        }
     }
-})
+    compare() {
+        //comparar memory con request y determinar si hace falta hacer algo
+
+    }
+    save() {
+        this.memory = this.newRequest;
+    }
+    erase() {
+        //borrar las clases, o la seccion si se necesita
+    }
+    action() {
+        //con lo que hay en memoria trabajar lo que haga falta
+    }
+}
+
+
+
